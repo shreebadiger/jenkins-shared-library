448d55e226355d8ec8e53047dafb6bf5cdbc935d
shree <shreesbadiger@gmail.com>
2023-12-25 11:15:29 +0530

    updated

---
+++ vars/ci.groovy
@@ -0,0 +1,32 @@
+def call ()
+
+pipeline {
+    agent {
+        node { label'workstation'}
+    }
+    stages{
+        stage('compiling') {
+            steps{
+              echo 'compiling'
+          }
+        }
+
+         stage('Test'){
+            steps{
+              echo 'Test'
+          }
+         }
+
+         stage('Build'){
+            steps{
+              echo 'Build'
+          }
+         }
+
+         stage('Release'){
+            steps{
+              echo 'Release'
+          }
+         }
+    }      
+}
