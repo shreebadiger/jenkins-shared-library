114fdab3ccc820505053ec9bcf522d2b9e6643da
shree <shreesbadiger@gmail.com>
2023-12-26 17:58:24 +0530

    updatetd

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -6,9 +6,24 @@ def call () {
             stage('Compile'){}
             stage('Build'){}
         }
-        
-    }
-    
+        else if (env.BRANCH_NAME ==~ "PR.*"){
+            stage('Code checkout'){}
+            stage('Compile'){}
+            stage('Test case'){}
+            stage('Integration test'){}
+        }
+        else if (env.TAG_NAME ==~ ".*"){
+            stage('Code checkout'){}
+            stage('Compile'){}
+            stage('Build'){}
+            stage('Release'){}
+        }
+        else {
+            stage('Code checkout'){}
+            stage('Compile'){}
+            stage('Test case'){}
+        }       
+    }    
 }
         //stage('Code checkout'){}
         //stage('Compile'){}
