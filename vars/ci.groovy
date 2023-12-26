da223dd4181d30962bf45bce5621be886f77ebd9
shree <shreesbadiger@gmail.com>
2023-12-26 17:52:59 +0530

    up

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -1,12 +1,18 @@
 def call () {
     node ('workstation') {
         sh 'env'
-        stage('Code checkout'){}
-        stage('Compile'){}
-        stage('Test case'){}
-        stage('Integration test'){}
-        stage('Build'){}
-        stage('Release'){}
+        if (env.BRANCH_NAME == "main") {
+            stage('Code checkout'){}
+            stage('Compile'){}
+            stage('Build'){}
+        }
+        
     }
     
 }
+        //stage('Code checkout'){}
+        //stage('Compile'){}
+        //stage('Test case'){}
+        //stage('Integration test'){}
+        //stage('Build'){}
+        //stage('Release'){}
