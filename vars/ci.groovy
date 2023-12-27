f8a5b3002e9718e90229a367e90c7cf6d584f28a
shree <shreesbadiger@gmail.com>
2023-12-27 17:57:25 +0530

    updated

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -3,14 +3,12 @@ def call () {
         sh "find . | sed -e '1d' | xargs rm -rf"
         if(env.TAG_NAME ==~ ".*") {
             env.branch_name = "refs/tags/${env.TAG_NAME}"
-        } else
-        {
-            if(env.BRANCH_NAME ==~ "PR-.*")
-            {
+        } else {
+            if(env.BRANCH_NAME ==~ "PR-.*") {
                 env.branch_name = "${env.CHANGE_BRANCH}"
             }
             else{
-                env.branch_name = ""${env.BRANCH_NAME}""
+                env.branch_name = "${env.BRANCH_NAME}"
             }
             env.branch_name = env.BRANCH_NAME
         }
