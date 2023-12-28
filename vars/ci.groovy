7e12c3816388084041843891d46007036cc715e0
shree <shreesbadiger@gmail.com>
2023-12-28 11:30:23 +0530

    updated

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -35,7 +35,7 @@ def call () {
             // sh 'npm test'
             }
             stage('Code Quality'){
-            print (AWS_SSM_PARAM(param_name: 'sonar.token'))
+            print AWS_SSM_PARAM('sonar.token')
             //env.SONAR_TOKEN = AWS_SSM_PARAM(param_name: 'sonar.token')
             //sh 'sonar-scanner -Dsonar.host.url=http://172.31.83.244:9000 -Dsonar.login=${SONAR_TOKEN} -Dsonar.projectKey=${repo_name}'
             }
