7f7e7ea8be89bdbc37dea169bdb2e8e1bb178959
shree <shreesbadiger@gmail.com>
2023-12-28 20:04:52 +0530

    updated

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -7,18 +7,19 @@ def call () {
         sh "find . | sed -e '1d' | xargs rm -rf"
         if(env.TAG_NAME ==~ ".*") {
             env.branch_name = "refs/tags/${env.TAG_NAME}"
-         } else {
-            if(env.BRANCH_NAME ==~ "PR-.*") {
+           } 
+        else {
+              if(env.BRANCH_NAME ==~ "PR-.*") {
                 env.branch_name = "${env.CHANGE_BRANCH}"
-            }
-            else{
+                }
+              else{
                 env.branch_name = "${env.BRANCH_NAME}"
-            }
-            env.branch_name = env.BRANCH_NAME
-        }
+                }
+                 env.branch_name = env.BRANCH_NAME
+             }
         stage('Code checkout'){
            // git branch: 'main', url: 'https://github.com/shreebadiger/expense-backend.git'
-           checkout scmGit(
+            checkout scmGit(
             branches: [[name : "${branch_name}"]],
             userRemoteConfigs: [[url:"https://github.com/shreebadiger/${repo_name}"]]
            )
@@ -27,7 +28,7 @@ def call () {
         if(app_type == "nodejs"){
         stage('Download dependencies'){
             sh 'npm install'
-        }
+             }
         }
         if (env.JOB_BASE_NAME ==~ "PR.*") {
             sh 'echo PR'
@@ -36,8 +37,8 @@ def call () {
             }
             stage('Code Quality'){
             env.SONAR_TOKEN = AWS_SSM_PARAM('sonar.token')
-              wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: "${SONAR_TOKEN}", var: 'PASSWORD']]]) {
-               sh 'sonar-scanner -Dsonar.host.url=http://172.31.83.244:9000 -Dsonar.login=${SONAR_TOKEN} -Dsonar.projectKey=${repo_name} -Dsonar.qualitygate.wait=true -Dsonar.exlusions=node_modules/**'
+            wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: "${SONAR_TOKEN}", var: 'PASSWORD']]]) {
+            sh 'sonar-scanner -Dsonar.host.url=http://172.31.83.244:9000 -Dsonar.login=${SONAR_TOKEN} -Dsonar.projectKey=${repo_name} -Dsonar.qualitygate.wait=true -Dsonar.exlusions=node_modules/**'
                }            
             }           
         }
@@ -54,13 +55,16 @@ def call () {
             stage('Release'){
                env.ARTIFACTORY_PASSWORD = AWS_SSM_PARAM('artifactory.password')
                wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: "${ARTIFACTORY_PASSWORD}", var: 'PASSWORD']]])
-               sh 'curl -sSf -u "admin:${ARTIFACTORY_PASSWORD}" -X PUT -T ${repo_name}-${TAG_NAME}.zip http://artifactory.sbadiger93.online:8081/artifactory/example-repo-local/${repo_name}-${TAG_NAME}.zip'
+               sh 'curl -sSf -u "admin:${ARTIFACTORY_PASSWORD}" -X PUT -T ${repo_name}-${TAG_NAME}.zip http://artifactory.sbadiger93.online:8081/artifactory/${repo_name}/${repo_name}-${TAG_NAME}.zip'
               }
         }
+    
         else {
             sh 'echo branch'
-            stage('Test case'){}
-            //sh 'npm test'
+            stage('Test case'){
+                 //sh 'npm test'
+            }
+           
         }       
     }    
 }
