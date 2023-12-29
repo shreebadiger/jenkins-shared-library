46f058dffeac8a6834045575a0127df509a7baaa
shree <shreesbadiger@gmail.com>
2023-12-30 01:41:58 +0530

    up

--- vars/goldenAmi.groovy
+++ vars/goldenAmi.groovy
@@ -1,17 +1,18 @@
 def call(){
     ansiColor('xterm'){
 
-        node('workstation'){
-        common.codeCheckout()
-        stage('Terraform Apply'){
+            node('workstation'){
+            common.codeCheckout()
+            stage('Terraform Apply'){
+                sh 'terraform init'
+            // sh 'terraform apply -auto-approve'
+            }
+            stage('Terraform Destroy'){
             sh 'terraform init'
-           // sh 'terraform apply -auto-approve'
+            sh 'terraform state rm aws_ami_from_instance.ami'
+            sh 'terraform destroy -auto-approve'
+            }
         }
-        stage('Terraform Destroy'){
-        sh 'terraform init'
-        sh 'terraform state rm aws_ami_from_instance.ami'
-        sh 'terraform destroy -auto-approve'
-        }
-     }
+      }
     }
-     }
+                               
